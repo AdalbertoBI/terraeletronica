a8ae2bc28f0e082287b66e4ee8a959c9222d7135
AdalbertoBI <adalbertobi@gmail.com>
2025-12-10 16:02:28 -0300

    v1.0.0.5.1 - Ajustes manuais na pagina Sensory Musical

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SITE_VERSION = '1.0.0.5.0';
-const CACHE_NAME = 'terra-eletronica-cache-v5.0';
+const SITE_VERSION = '1.0.0.5.1';
+const CACHE_NAME = 'terra-eletronica-cache-v5.1';
 
 const SCOPE_URL = new URL('./', self.location);
 const resolveScopeUrl = path => {
