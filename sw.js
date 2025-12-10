fbc347fa1f3e5ae8c23c4d63e8029c94ea8715ce
AdalbertoBI <adalbertobi@gmail.com>
2025-12-10 18:45:09 -0300

    v1.0.0.5.6 - Fix: Botoes navegacao carrossel funcionando

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SITE_VERSION = '1.0.0.5.5';
-const CACHE_NAME = 'terra-eletronica-cache-v5.5';
+const SITE_VERSION = '1.0.0.5.6';
+const CACHE_NAME = 'terra-eletronica-cache-v5.6';
 
 const SCOPE_URL = new URL('./', self.location);
 const resolveScopeUrl = path => {
